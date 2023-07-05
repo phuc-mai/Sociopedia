2ac545446fb6741e0a56099a06597496204147ef
Phuc Mai <mariamaihong1309@gmail.com>
2023-07-05 17:02:49 -0400

    first commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,64 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    /* Create a new user*/
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    /* Save the new user to the database */
+    const savedUser = await newUser.save();
+
+    /* Send a success response */
+    res.status(200).json({ message: 'User registered successfully', user: savedUser });
+  } catch (err) {
+    /* Handle any errors that occur during registration */
+    res.status(500).json({ message: 'Registration failed', error: err.message });
+  }
+};
+
+
+/* LOG IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ message: "User does not exist." });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials." });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
