7e5edb9552ddae0a95a207c9726c2672eff53d5f
Phuc Mai <mariamaihong1309@gmail.com>
2023-07-10 10:14:21 -0400

    back-end

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -17,14 +17,14 @@ export const register = async (req, res) => {
     } = req.body;
 
     const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     /* Create a new user*/
     const newUser = new User({
       firstName,
       lastName,
       email,
-      password: passwordHash,
+      password: hashedPassword,
       picturePath,
       friends,
       location,
@@ -53,6 +53,7 @@ export const login = async (req, res) => {
     if (!user) return res.status(400).json({ message: "User does not exist." });
 
     const isMatch = await bcrypt.compare(password, user.password);
+
     if (!isMatch) return res.status(400).json({ message: "Invalid credentials." });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js"
+import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
 import { register } from "./controllers/auth.js"
+import { createPost } from "./controllers/posts.js"
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -38,9 +42,10 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
+
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-// app.post("/posts", verifyToken, upload.single("picture"), createPost);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes); // for Login
