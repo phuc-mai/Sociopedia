86b09f4d6a1edfd9d1bf96c641ff23d2128b7fd2
Phuc Mai <mariamaihong1309@gmail.com>
2023-07-12 12:25:57 -0400

    redux setup & navbar

---
+++ client/src/scenes/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material" 
+import Navbar from "components/Navbar"
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  )
+}
+
+export default HomePage
